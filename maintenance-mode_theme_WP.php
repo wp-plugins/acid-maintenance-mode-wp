5ce6b3c62eacc2b43f6c63f9ea0abad8cd0fbdaa
acidc00l <acidc00l@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 13:40:23 +0000

    version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/acid-maintenance-mode-wp/tags/2.0@442106 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- maintenance-mode_theme_WP.php
+++ maintenance-mode_theme_WP.php
@@ -6,12 +6,12 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<title><?php bloginfo('name'); ?> &raquo; <?php echo $this->g_opt['mamo_pagetitle']; ?></title>
-<link rel="stylesheet" href="<?php echo get_settings('home'); ?>/wp-content/plugins/modo-mantenimiento-60/maintenance-mode_theme_WP_files/style.css" type="text/css" media="all" />
+<link rel="stylesheet" href="<?php echo get_settings('home'); ?>/wp-content/plugins/acid-maintenance-mode-wp/maintenance-mode_theme_WP_files/style.css" type="text/css" media="all" />
 
-<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/modo-mantenimiento-60/maintenance-mode_theme_WP_files/js/jquery-1.3.2.min.js"></script>
-<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/modo-mantenimiento-60/maintenance-mode_theme_WP_files/js/cufon.js"></script>
-<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/modo-mantenimiento-60/maintenance-mode_theme_WP_files/js/Myriad_Pro_300.font.js"></script>
-<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/modo-mantenimiento-60/maintenance-mode_theme_WP_files/js/Myriad_Pro_700.font.js"></script>
+<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/acid-maintenance-mode-wp/maintenance-mode_theme_WP_files/js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/acid-maintenance-mode-wp/maintenance-mode_theme_WP_files/js/cufon.js"></script>
+<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/acid-maintenance-mode-wp/maintenance-mode_theme_WP_files/js/Myriad_Pro_300.font.js"></script>
+<script type="text/javascript" src="<?php echo get_settings('home'); ?>/wp-content/plugins/acid-maintenance-mode-wp/maintenance-mode_theme_WP_files/js/Myriad_Pro_700.font.js"></script>
 <script type="text/javascript">
 Cufon.replace('.#head h2', { fontFamily: 'Myriad Light' });
 Cufon.replace('#head h1,#subhead h3', { fontFamily: 'Myriad Pro' });
